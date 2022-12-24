c81b0a6bef04c0f7d72be368086cb48d243416bf
vadism3d <72330747+vadism3d@users.noreply.github.com>
2022-12-24 20:26:13 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const el = (selector) => document.querySelector(selector);
+
+
+        // при нажатии на кнопку
+        el('#generateQRCode').addEventListener('click', function() {
+           
+            // получаем блок куда будет выводиться QR-код
+            let qrCodeOutput = document.querySelector('#qrCodeOutput');
+
+            // получаем содержимое текстового поля
+            let text = el('#qrCodeText').value;
+
+            // очищаем блок с QR-кодом
+            document.getElementsByClassName('qrCodeOutput').innerHTML = " ";
+            // qrCodeOutput.innerHTML = " ";
+
+            // и помещаем в него сгенерированный библиотекой QR-код
+            qrCodeOutput.append(QRCode.generateHTML(text, {}))
+        });
